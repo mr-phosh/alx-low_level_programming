21e41bc3a6983d551c699f7ee8a028cbd2d6792b
mr-phosh <mrphosh@gmail.com>
2023-09-13 11:18:48 -0700

    redone

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,30 +1,24 @@
 #include "function_pointers.h"
-#include <stdio.h>
+	#include <stdio.h>
+	/**
+	 * array_iterator - prints each array elem on a newl
+	 * @array: array
+	 * @size: how many elem to print
+	 * @action: pointer to print in regular or hex
+	 * Return: void
+	 */
+	void array_iterator(int *array, size_t size, void (*action)(int))
+	{
+		unsigned int i;
 
-void array_iterator(int *array, size_t size, void (*action)(int)) {
-    if (array == NULL || action == NULL) {
-        return; // Safety check to ensure array and action are valid
-    }
 
-    for (size_t i = 0; i < size; i++) {
-        action(array[i]); // Call the action function on each element of the array
-    }
-}
+		if (array == NULL || action == NULL)
+			return;
 
-// Example action function: Print an integer
-void print_int(int value) {
-    printf("%d ", value);
-}
 
-int main() {
-    int arr[] = {1, 2, 3, 4, 5};
-    size_t arr_size = sizeof(arr) / sizeof(arr[0]);
-
-    // Call array_iterator with the print_int function to print each element
-    array_iterator(arr, arr_size, print_int);
-
-    printf("\n");
-
-    return 0;
-}
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
 
